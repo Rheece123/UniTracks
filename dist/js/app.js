38fcba8dff500f78088519faa85dc89974f8c011
rheece123 <rheece.d123@gmail.com>
2020-01-17 12:47:06 +0000

    Added class to prevent transitions happening on page load. Added function to JS to remove this class after page load so that transitions still work

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,18 +1,25 @@
+// Prevent transition on page load
+document.addEventListener('DOMContentLoaded', function() {
+	let node = document.querySelector('.preload-transitions');
+	node.classList.remove('preload-transitions');
+});
+
+// Search Functionality on Tracks Page
 const trackInput = document.querySelector('#search-courses');
 
 trackInput.addEventListener('keyup', filterCourses);
 
 function filterCourses(e) {
-  const inputText = e.target.value.toLowerCase();
-  const tracks = document.querySelectorAll('.track-text');
+	const inputText = e.target.value.toLowerCase();
+	const tracks = document.querySelectorAll('.track-text');
 
-  tracks.forEach(function(track) {
-    const trackText = track.textContent;
+	tracks.forEach(function(track) {
+		const trackText = track.textContent;
 
-    if (trackText.toLowerCase().indexOf(inputText) !== -1) {
-      track.parentElement.style.display = 'grid';
-    } else {
-      track.parentElement.style.display = 'none';
-    }
-  });
+		if (trackText.toLowerCase().indexOf(inputText) !== -1) {
+			track.parentElement.style.display = 'grid';
+		} else {
+			track.parentElement.style.display = 'none';
+		}
+	});
 }
