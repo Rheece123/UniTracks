e84c355452528562659485540efc1d508f556a94
rheece123 <rheece.d123@gmail.com>
2020-02-05 14:55:45 +0000

    Removed search button on tracks.html and fixed famicon link href issue on all pages

--- dist/js/search/main.js
+++ dist/js/search/main.js
@@ -1,14 +1,18 @@
+// UI Variables
+const trackInput = document.querySelector('#search-courses');
+
+// EVENTS
+
+// Search Functionality on Tracks Page
+trackInput.addEventListener('keyup', filterCourses);
+
 // Prevent transition on page load
 document.addEventListener('DOMContentLoaded', function() {
 	let node = document.querySelector('.preload-transitions');
 	node.classList.remove('preload-transitions');
 });
 
-// Search Functionality on Tracks Page
-const trackInput = document.querySelector('#search-courses');
-
-trackInput.addEventListener('keyup', filterCourses);
-
+// Filter through courses
 function filterCourses(e) {
 	const inputText = e.target.value.toLowerCase();
 	const tracks = document.querySelectorAll('.track-text');
