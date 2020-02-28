977cecd4057fdb1af5d63d1ba2abaf19cb6434ec
rheece123 <rheece.d123@gmail.com>
2020-02-28 21:19:49 +0000

    Added code to go to database to get questions for assessment rather than object

--- dist/includes/assessment.inc.php
+++ dist/includes/assessment.inc.php
@@ -2,7 +2,59 @@
 
 session_start();
 
-// Check for a POST Variable
+// Check for a POST Variable of score at end of assessment
+if (isset($_POST['currentQuestion'])) {
+  require 'dbh.inc.php';
+
+  // Username from Session
+  $username = $_SESSION['userName'];
+
+  // AJAX Params
+  $questionID = mysqli_real_escape_string($conn, $_POST['currentQuestion']);
+  $moduleTitle = mysqli_real_escape_string($conn, $_POST['moduleTitle']);
+
+  // Create table name by replacing space with underscore
+  $table = strtolower(str_replace(' ', '_', $moduleTitle));
+
+  // Select question from table using id
+  $sql = "SELECT * FROM $table WHERE id = ?";
+  $stmt = mysqli_stmt_init($conn);
+
+  // If SQL statement cannot be prepared
+  if (!mysqli_stmt_prepare($stmt, $sql)) {
+    header("Location: ../assessment.php?error=sqlerror");
+    exit();
+  }
+  else {
+    // Bind the placeholder to the question id
+    mysqli_stmt_bind_param($stmt, "i", $questionID);
+
+    // Execute prepared statement
+    mysqli_stmt_execute($stmt);
+
+    // Store result from prepared statement
+    mysqli_stmt_store_result($stmt);
+
+    // Store query results in an array
+    if (mysqli_stmt_fetch) {
+      echo json_encode(array($question = "question",
+                             $answer_a = "answer_a",
+                             $answer_b = "answer_b",
+                             $answer_c = "answer_c",
+                             $answer_d = "answer_d",
+                             $correct_answer = "correct_answer",
+                             $category = "category"          
+                            ));
+    }
+
+    
+    
+  }
+}
+
+
+
+// Check for a POST Variable of score at end of assessment
 if (isset($_POST['score'])) {
   require 'dbh.inc.php';
 
