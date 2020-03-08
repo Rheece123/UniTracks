ec4e33857c2551a38aaee253885a2ff0b64b177a
rheece123 <rheece.d123@gmail.com>
2020-03-08 09:08:33 +0000

    Added echo output to db connection for debugging SQL error

--- public/includes/dbh.inc.php
+++ public/includes/dbh.inc.php
@@ -9,5 +9,9 @@ $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
 // If db connection failed, kill the connection and output an error message
 if (!$conn) {
+  echo "Error: Unable to connect to MySQL." . PHP_EOL;
+  echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+  echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+  exit;
   die("Connection failed: ".mysql_connect_error());
 }
