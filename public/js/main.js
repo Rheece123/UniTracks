1c853fd79062ccaea6ff83cc88b5979da4fef07a
rheece123 <rheece.d123@gmail.com>
2020-03-13 11:52:13 +0000

    Added JS which removes the outline when mouse is being used but adds the outline if keyboard is being used

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	let node = document.querySelector('.preload-transitions');
 	node.classList.remove('preload-transitions');
 });
+
+// Only show the outline on an element when the tab key is pressed, otherwise don't show any outline
+document.body.addEventListener('mousedown', function() {
+	document.body.classList.add('using-mouse');
+});
+
+// Re-enable focus styling when Tab is pressed
+document.body.addEventListener('keydown', function(event) {
+	if (event.keyCode === 9) {
+		document.body.classList.remove('using-mouse');
+	}
+});
